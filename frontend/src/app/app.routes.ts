1b9567dead086b8b8da7946fa38f2818681266b1
Diestroid <diegoarevalo2005@gmail.com>
2025-05-21 13:42:36 -0500

    login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent} from './home/home.component'
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {path: 'about-us', component: AboutUsComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
