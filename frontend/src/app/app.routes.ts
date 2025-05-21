f34ad2e1466a8ec6d7aa34c70c84865bf1e85907
Diestroid <diegoarevalo2005@gmail.com>
2025-05-21 07:13:37 -0500

    front-end aboutus, home, footer, navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent} from './home/home.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'about-us', component: AboutUsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
