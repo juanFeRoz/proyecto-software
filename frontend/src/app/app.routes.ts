bcb3387b4f2c987910a7f3f1dad9b12352ed0d44
Diestroid <diegoarevalo2005@gmail.com>
2025-05-21 15:17:31 -0500

    Sign Up funcional

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent} from './home/home.component'
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
   {path: 'about-us', component: AboutUsComponent},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  name: string = '';
+  username: string = '';
+  email: string = '';
+  password: string = '';
 
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+
+  onSignup() {
+    const userData = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    }
+    
+    this.http.post('http://localhost:8080/api/auth/signup', userData, { responseType: 'text' })
+      .subscribe({
+        next: response => {
+          alert('Registro exitoso');
+          console.log(response);
+        },
+        error: error => {
+          alert('Error al registrar');
+          console.error(error);
+        }
+      });
+  }
 }
