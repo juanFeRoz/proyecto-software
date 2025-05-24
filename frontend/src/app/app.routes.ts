81a1d1a0613c711475c0917084d5ad249d563983
BrayanQuintero123 <brayan2221707@correo.uis.edu.co>
2025-05-24 16:40:34 -0500

    chat funcional

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent} from './home/home.component'
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { ChatComponent } from './chat/chat.component';
 
 export const routes: Routes = [
   {path: 'about-us', component: AboutUsComponent},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
+  {path: 'chat', component: ChatComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
