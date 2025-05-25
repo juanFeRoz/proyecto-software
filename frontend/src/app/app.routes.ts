71eb1f958ca0ff7a5c00b178c13f178baaf07942
Diestroid <diegoarevalo2005@gmail.com>
2025-05-25 11:53:06 -0500

    ToDoList Funcional

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent} from './home/home.component'
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ChatComponent } from './chat/chat.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 export const routes: Routes = [
   {path: 'about-us', component: AboutUsComponent},
@@ -11,5 +12,6 @@ export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'chat', component: ChatComponent},
+  {path: 'to-do-list', component: ToDoListComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
---
+++ frontend/src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-to-do-list',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './to-do-list.component.html',
+  styleUrl: './to-do-list.component.css'
+})
+export class ToDoListComponent {
+  newTask = '';
+  tasks: { name: string; completed: boolean }[] = [];
+
+  addTask() {
+    if (this.newTask.trim()) {
+      this.tasks.push({ name: this.newTask.trim(), completed: false });
+      this.newTask = '';
+    }
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
