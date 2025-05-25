1f253010971c963f5ed27eacbb452de26d2a9326
Juan Felipe Rojas <juanfeliperojas2@protonmail.com>
2025-05-24 22:30:03 -0500

    reiniciar el back

--- backend/src/main/java/org/example/backend/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package org.example.backend.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-    private String usernameOrEmail;
-    private String password;
-}
--- backend/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO roles(name) VALUES( 'ROLE_CUSTOMER');
-INSERT INTO roles(name) VALUES( 'ROLE_ADMIN');
